9ef15c7f55f60e926eb89cc64fa82066c76c57a1
Jean-François Tronche <jftronche@univ-orleans.fr>
2019-09-13 16:08:08 +0200

    Modele

---
+++ book.py
@@ -0,0 +1,12 @@
+class book():
+    _title = ""
+    _author = ""
+    _description = ""
+    _book_url = ""
+    _image_url = ""
+    _creation_date = None
+    _publication_date = None
+
+
+
+    pass
